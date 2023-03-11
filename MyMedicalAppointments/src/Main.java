fc425c86363b3751c7e706e8481d12b0ee39cf85
pepinadas <mmmunapregunta@gmail.com>
2023-03-11 15:00:33 -0600

    Agregando parte de el proyecto desde la carpeta acronima

---
+++ MyMedicalAppointments/src/Main.java
@@ -0,0 +1,12 @@
+import model.Doctor;
+import model.ISchedulable;
+import model.Patient;
+import model.User;
+import java.util.Date;
+import static UI.UIMenu.*;
+
+public class Main {
+    public static void main(String[] args) {
+        showMenu();
+    }
+}
---
+++ MyMedicalAppointments/src/model/Doctor.java
@@ -0,0 +1,93 @@
+package model;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Doctor extends User {
+    //Atributos
+
+    private String speciality;
+    private ArrayList<AvaiableAppointment> avaiableAppointments = new ArrayList<>();
+
+    public Doctor(String name, String email) {
+        super(name, email);
+    }
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+
+    public void addAvailableAppointment(String date, String time) {
+        avaiableAppointments.add(new AvaiableAppointment(date, time));
+    }
+    public ArrayList<AvaiableAppointment> getAvaiableAppointments() {
+        return avaiableAppointments;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nSpeciality: " + speciality + "\nAvailable " + avaiableAppointments.toString();
+    }
+
+    @Override
+    public void showDataUser() {
+        System.out.println("Empleado del Hospital: Cruz Roja");
+        System.out.println("Departamento: Cancerología");
+    }
+
+    public static class AvaiableAppointment{
+        private int id;
+        private Date date;
+        private String time;
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+
+        public AvaiableAppointment(String date, String time) {
+            try {
+                this.date = format.parse(date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate(String DATE) {
+            return date;
+        }
+        public String getDate() {
+            return format.format((date));
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+
+        @Override
+        public String toString() {
+            return "AvaiableAppointments \nDate: " + date +"\nTime:" + time;
+        }
+    }
+
+}
+
